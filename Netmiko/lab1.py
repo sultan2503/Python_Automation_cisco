ffce89fe069a7f5aa7763821367842fdd2232424
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2020-12-09 09:22:43 +0530

    Create lab1.py

---
+++ Netmiko/lab1.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+
+R1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.1.202',
+    'username': 'ccna',
+    'password': 'ccna'
+}
+
+net_connect = ConnectHandler(**R1)
+output = net_connect.send_command('show ip int brief')
+print (output)
