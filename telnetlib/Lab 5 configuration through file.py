c5eb3b5462268620680da1b9c961d2ee8e6952b0
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2021-02-14 10:02:26 +0530

    Update Lab 5 configuration through file.py

--- telnetlib/Lab 5 configuration through file.py
+++ telnetlib/Lab 5 configuration through file.py
@@ -1,1 +1,22 @@
+import getpass
+import telnetlib
 
+HOST = "192.168.1.10"
+user = input("Enter your Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+f=open("conf.txt","+r")
+for i in f:
+    tn.write(b" "+i.encode('ascii').strip()+b"\n")
+
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
