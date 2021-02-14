9517af525ef116409c1652284309d0e24ba0fa1b
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2021-02-14 09:41:23 +0530

    Create Lab1.py
    Get interface information

---
+++ telnetlib/Lab1.py
@@ -0,0 +1,18 @@
+import telnetlib
+
+HOST = "192.168.1.10"
+user = "ccna"
+password = "ccna"
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"sh ip int br\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
