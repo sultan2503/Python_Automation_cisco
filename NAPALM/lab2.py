30f540c7ef368ce09ed80a67cf97a0ee02122a71
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2021-02-17 18:23:15 +0530

    Update lab2.py

--- NAPALM/lab2.py
+++ NAPALM/lab2.py
@@ -3,6 +3,6 @@ driver = get_network_driver('ios')
 device = driver('192.168.1.109', 'ccna', 'ccna')
 device.open()
 
-device.load_replace_candidate(filename='/home/kali/Desktop/test.conf')
+device.load_merge_candidate(filename='/home/kali/Desktop/test.conf')
 device.commit_config()
 device.close()
