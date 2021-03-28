dce0a840e84e48905b5dad2bd9679a66f96e963e
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2021-03-28 11:42:09 +0530

    Update config.py

--- NAPALM/config.py
+++ NAPALM/config.py
@@ -3,13 +3,15 @@ driver = get_network_driver('ios')
 device = driver('192.168.1.109', 'ccna', 'ccna')
 device.open()
 
-device.load_replace_candidate(filename='/home/kali/Desktop/test.conf')
+device.load_merge_candidate(filename='/home/kali/Desktop/test.cfg')
 diff= device.compare_config()
-if diff == 0:
+if len(diff) == 0:
 	device.discard_config()
 	print("Nothing to change")
+	print(diff)
 else:
 	device.commit_config()
 	print("success")
+	print(diff)
 
 device.close()
